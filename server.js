fdb5c620856dad7d9ecb226ad72ec1fe0e037817
Amandine <amandine2.divay@gmail.com>
2020-02-13 10:48:59 +0100

    add token into request

--- server.js
+++ server.js
@@ -20,36 +20,35 @@ let List = require('./models/list');
 let Task = require('./models/task')
 
 // Déclarations des routes de notre application : 
-app.route('/').get(function(req, res) {
+app.route('/').get(function(req, res){
     res.send('hello world !');
 });
 
 // Route register + requête : 
 app.route ('/register').post(function(req, res){
-    bcrypt.hash(req.body.password, 10, function(err, hash) {
+    bcrypt.hash(req.body.password, 10, function(err, hash){
         let user = new User ({
-            firstname:  req.body.firstname, 
-            lastname:  req.body.lastname, 
+            firstname: req.body.firstname, 
+            lastname: req.body.lastname, 
             password: hash, 
-            email:  req.body.email, 
+            email: req.body.email, 
             listId: req.body['listId[]']
-            }); 
+        }); 
         user.save(function(err, data){
             if(err)
-            res.send(err)
+                res.send(err)
             else{
                 res.send(data); 
-            }
+            };
         }); 
     }); 
-    // res.send('Salut')
 });
 
 // Route connexion + requête : 
 app.route('/connexion').post(function(req, res){
     User.findOne({email: req.body.email}, function(err, data){
         if (data){
-            bcrypt.compare(req.body.password, data.password, function(err, result) {
+            bcrypt.compare(req.body.password, data.password, function(err, result){
                 if (result){
                     let token = jwt.sign({id: data._id}, "maclefsecrete");  //pas de données sensibles dans le token, voir jws sinon
                     let response = {user: data, token: token}; 
@@ -58,7 +57,7 @@ app.route('/connexion').post(function(req, res){
                 else
                     res.send('error : ' + err);
                 }); 
-        } 
+        }
         else 
             res.send(err);
     }); 
@@ -81,36 +80,48 @@ app.route('/users').get(function(req, res){
 
 // Route pour mettre à jour l'user + requête : 
 app.route('/userupdate').put(function(req, res){
-    User.updateOne({ _id: req.body.id }, { $set: { firstname: req.body.firstname } }, function(err, data){
-        if(err)
-        res.send(err)
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
+        if (err)
+                res.send(err)
         else{
-            res.send(data); 
+            User.updateOne({ _id: [decoded.id ]}, { $set: { firstname: req.body.firstname } }, function(err, data){
+                if(err)
+                    res.send(err)
+                else{
+                    res.send(data); 
+                };
+            });
         };
-    });
     }); 
+}); 
 
 // Route pour supprimer l'user + requête : 
 app.route('/deleteuser').delete(function(req, res){
-    User.deleteOne({_id: req.body.id}, function(err, data){
-        if(err)
-        res.send(err)
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
+        if (err)
+                res.send(err)
         else{
-            res.send(data); 
-        };
-    })
+            User.deleteOne({_id: [decoded.id]}, function(err, data){
+                if(err)
+                        res.send(err)
+                else{
+                    res.send(data); 
+                };
+            }); 
+        }; 
+    });  
 }); 
 
 // Route créer une liste + requête : 
 app.route('/newlist').post(function(req, res){
     jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
         if (err)
-            res.send(err)
+                res.send(err)
         else{
             let list = new List({
                 namelist: req.body.namelist,
                 userId: [decoded.id]
-            })
+            });
             list.save(function(err, data){
                 if(err)
                     res.send(err)
@@ -118,8 +129,7 @@ app.route('/newlist').post(function(req, res){
                     res.send(data)
                 }; 
             }); 
-            // res.send('Une liste')
-        }
+        };
     }); 
 }); 
 
@@ -143,91 +153,133 @@ app.route('/findlist').get(function(req, res){
 
 // Route pour chercher une liste dans un user + requête (recherche par id) : 
 app.route('/userbyid/:id').get(function(req, res){
-    User.findOne({_id: req.params.id}).populate('list').exec(function(err, data){
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
         if (err)
             res.send(err)
         else{
-            res.send(data)
-    }; 
+            User.findOne({_id: [decoded.id]}).populate('list').exec(function(err, data){
+                if (err)
+                    res.send(err)
+                else{
+                    res.send(data)
+                }; 
+            }); 
+        };
     }); 
 }); 
 
 // Route pour mettre à jour une liste + requête :  
 app.route('/listupdate').put(function(req, res){
-    List.updateOne({_id: req.body.id}, {$set: { namelist: req.body.namelist} }, function(err, data){
-        if(err)
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
+        if (err)
             res.send(err)
         else{
-            res.send(data); 
+            List.updateOne({_id: [decoded.id]}, {$set: { namelist: req.body.namelist} }, function(err, data){
+                if(err)
+                    res.send(err)
+                else{
+                    res.send(data); 
+                };
+            });
         };
-    })
+    });
 }); 
 
 // Route supprimer une liste + requête : 
 app.route('/deletelist').delete(function(req, res){
-    List.deleteOne({_id: req.body.id}, function(err, data){
-        if(err)
-        res.send(err)
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
+        if (err)
+            res.send(err)
         else{
-            res.send(data); 
+            List.deleteOne({_id: [decoded.id]}, function(err, data){
+                if(err)
+                    res.send(err)
+                else{
+                    res.send(data); 
+                };
+            });
         };
-    })
+    });
 }); 
 
 // Route pour créer une tâche + requête : 
 app.route('/createtask').post(function(req, res){
-    let task = new Task({ 
-        nametask: req.body.nametask, 
-        listId: req.body['listId[]'],
-    })
-    task.save(function(err, data){
-        if(err)
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
+        if (err)
             res.send(err)
         else{
-            res.send(data)
-        }; 
-    }); 
+            let task = new Task({ 
+                nametask: req.body.nametask, 
+                listId: [decode.id],
+            })
+            task.save(function(err, data){
+                if(err)
+                    res.send(err)
+                else{
+                    res.send(data)
+                }; 
+            });
+        };
+    });
 }); 
 
 // Route pour chercher toutes les tâches + requête : 
 app.route('/findtask').get(function(req, res){
     Task.find({}, function(err, data){
         res.send(data); 
-    }); 
+    });
 }); 
 
 // Route pour trouver une list dans tâche + requête (recherche par id) : 
 app.route('/taskbyid/:id').get(function(req, res){
-    Task.findOne({_id: req.params.id}).populate('List').exec(function(err, data){
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
         if (err)
             res.send(err)
         else{
-            res.send(data)
-    }; 
-    }); 
+            Task.findOne({_id: [decoded.id]}).populate('List').exec(function(err, data){
+                if (err)
+                    res.send(err)
+                else{
+                    res.send(data)
+                }; 
+            }); 
+        };
+    });
 }); 
 
 
 // Route pour mettre à jour une tâche + requête : 
 app.route('/updatetask').put(function(req, res){
-    Task.updateOne({_id: req.body.id}, {$set: { nametask: req.body.nametask} }, function(err, data){
-        if(err)
-        res.send(err)
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
+        if (err)
+            res.send(err)
         else{
-            res.send(data); 
+            Task.updateOne({_id: [decoded.id]}, {$set: { nametask: req.body.nametask} }, function(err, data){
+                if(err)
+                    res.send(err)
+                else{
+                    res.send(data); 
+                };
+            });
         };
-    })
+    });
 }); 
 
 // Route pour supprimer une tâche + requête : 
 app.route('/deletetask').delete(function(req, res){
-    Task.deleteOne({_id: req.body.id}, function(err, data){
-        if(err)
-        res.send(err)
+    jwt.verify(req.headers["x-access-token"],"maclefsecrete", function(err, decoded){
+        if (err)
+            res.send(err)
         else{
-            res.send(data); 
+            Task.deleteOne({_id: [decoded.id]}, function(err, data){
+                if(err)
+                    res.send(err)
+                else{
+                    res.send(data); 
+                };
+            });
         };
-    })
+    });
 }); 
 
 // Mise en écoute de notre application (sur le port 3000)
